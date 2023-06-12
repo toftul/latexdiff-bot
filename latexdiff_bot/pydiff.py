5c24061b0ba99bd598978a12f85045ca05581a89
Ivan <toftul.ivan@gmail.com>
2023-06-13 09:15:02 +1000

    in progress

--- latexdiff_bot/pydiff.py
+++ latexdiff_bot/pydiff.py
@@ -3,11 +3,43 @@
 
 import argparse
 import os 
+import subprocess
+
+from settings import *
 
 
-def do_pydiff(path_to_oldtex, path_to_newtex, if_compile=True, if_fast=False, if_imagediff=False):
+def do_pydiff(path_to_oldtex, path_to_newtex, compile=True, fast=False, imagediff=False, time_limit=DEFAULT_COMPILE_TIME_LIMIT, difftexname=DEFAULT_DIFF_TEX_NAME):
+    # make diff.tex 
+    newtex_dir = os.path.dirname(path_to_newtex)
+    path_to_difftex = os.path.join(newtex_dir, difftexname)
+    # TODO
+    # https://tex.stackexchange.com/a/598735/249682
+    # to make flatten work properly
+    latexdiff_command = f'latexdiff --flatten {path_to_oldtex} {path_to_newtex} > {path_to_difftex}'
+    subprocess.run(
+        latexdiff_command, 
+        shell=True, 
+        #check=True
+    )
 
-    return 0
+    path_to_diffpdf = path_to_difftex.rsplit('.', 1)[0] + '.pdf'
+    if compile:
+        # Compile the diff.tex to diff.pdf
+        latexmk_command = f'latexmk -interaction=nonstopmode -cd -f -pdf {path_to_difftex}'
+        try:
+            subprocess.run(
+                latexmk_command, 
+                shell=True,
+                timeout=time_limit,  # s 
+                #check=True
+            )
+        except subprocess.TimeoutExpired:
+            print("Timeout!")
+        except subprocess.CalledProcessError:
+            print("Something went wrong.")
+        else:
+            print("The diff.pdf is ready!")
+
 
 
 if __name__ == "__main__":
@@ -20,6 +52,8 @@ if __name__ == "__main__":
     parser.add_argument("--compile", action="store_true", help="Flag to trigger compilation")
     parser.add_argument("--fast", action="store_true", help="Flag to compile without links and figures")
     parser.add_argument("--imagediff", action="store_true", help="Flag to trigger image comparison tool")
+    parser.add_argument("--timelimit", help=f'Time limit in seconds (default {DEFAULT_COMPILE_TIME_LIMIT} sec)', required=False)
+    parser.add_argument("--difftexname", help=f'Diff .tex filename (default {DEFAULT_DIFF_TEX_NAME})', required=False)
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -28,8 +62,10 @@ if __name__ == "__main__":
     do_pydiff(
         path_to_oldtex=args.oldtex,
         path_to_newtex=args.newtex,
-        if_compile=args.compile,
-        if_fast=args.fast,
-        if_imagediff=args.imagediff
+        compile=args.compile,
+        fast=args.fast,
+        imagediff=args.imagediff,
+        time_limit=args.timelimit,
+        difftexname=args.difftexname
     )
 
--- latexdiff_bot/settings.py
+++ latexdiff_bot/settings.py
@@ -1,5 +1,24 @@
 import json
 
+# default names across the project
+DEFAULT_NEW_TEX = 'main_new.tex'
+DEFAULT_OLD_TEX = 'main_old.tex'
+
+DEFAULT_OLD_ZIP = 'old.zip'
+DEFAULT_NEW_ZIP = 'new.zip'
+
+DEFAULT_NEW_DIR = 'new'
+DEFAULT_OLD_DIR = 'old'
+
+USER_CONFIG_NAME = 'user_settings.json'
+USER_DATA_DIR = 'user_data'
+
+DEFAULT_COMPILE_TIME_LIMIT = 30  # s
+
+DEFAULT_DIFF_TEX_NAME = "diff.tex"
+DEFAULT_DIFF_PDF_NAME = DEFAULT_DIFF_TEX_NAME.rsplit('.', 1)[0] + '.pdf'
+
+
 def load_json_file(filename):
     try:
         with open(filename, 'r') as file:
